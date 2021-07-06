592119486bafde064be5563a8fb56a104dceb26e
leguia <leandro_eguia@hotmail.com>
2021-07-06 18:55:55 -0300

    Creacion de la estructura del proyecto

--- src/main/java/com/project/market/web/controller/HolaMundoController.java
+++ src/main/java/com/project/market/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.project.market;
+package com.project.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
